9ac87e06320a193ab01c558b21551b96370a0b4a
Nguyen Trung Kien <ntkien651@gmail.com>
2024-02-11 14:40:29 +0700

    Thêm bộ lọc cho api task

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const Task = require("../../../models/task.model");
 
-router.get("/", async (req, res) => {
-  const task = await Task.find({
-    deleted: false,
-  });
-  res.json(task);
-});
-router.get("/detail/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const task = await Task.find({
-      _id: id,
-      deleted: false,
-    });
-    res.json(task);
-  } catch (error) {
-    res.json("fail");
-  }
-});
+const controller = require("../controllers/task.controller");
+//[GET] /api/v1/tasks
+router.get("/",controller.index);
+//[GET] /api/v1/tasks/detail:id
+router.get("/detail/:id", controller.detail);
 module.exports = router;
