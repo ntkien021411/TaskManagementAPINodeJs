1bcf0cf14f0cbf2b95e09f368e74777eaf785f26
Nguyen Trung Kien <ntkien651@gmail.com>
2024-02-11 20:39:58 +0700

    Thêm CORS

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+
+//CORS
+const cors = require('cors');
+app.use(cors());
+
+
 //Body-Parser
 var bodyParser = require('body-parser')
+// parse application/json
+app.use(bodyParser.json())
+
+
 // ENV
 require("dotenv").config();
 const port = process.env.PORT;
@@ -10,8 +20,7 @@ const port = process.env.PORT;
 const database = require("./config/database");
 database.connect();
 
-// parse application/json
-app.use(bodyParser.json())
+
 
 const routesApiVer1 = require("./api/v1/routes/index.route")
 routesApiVer1(app);
