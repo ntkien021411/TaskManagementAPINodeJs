381bb628fef87a174fe45204be98b1feb8961599
Nguyen Trung Kien <ntkien651@gmail.com>
2024-02-11 16:39:44 +0700

    change status multi task API

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -59,7 +59,7 @@ module.exports.detail = async (req, res) => {
   }
 };
 
-//[GET] /api/v1/tasks/changeStatus/:id
+//[PATCH] /api/v1/tasks/change-status/:id
 module.exports.changeStatus = async (req, res) => {
   try {
     const id = req.params.id;
@@ -76,6 +76,40 @@ module.exports.changeStatus = async (req, res) => {
       code: 200,
       message: "Cập nhật trạng thái thành công!",
     });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!",
+    });
+  }
+};
+
+//[PATCH] /api/v1/tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+  try {
+    const {ids,key,value} = req.body;
+    switch (key) {
+      case "status":
+        await Task.updateMany(
+          {
+            _id: {$in:ids}
+          },
+          {
+            status: value
+          }
+        );
+        res.json({
+          code: 200,
+          message: "Cập nhật trạng thái thành công!",
+        });
+        break;
+      default:
+        res.json({
+          code: 400,
+          message: "Không tồn tại!",
+        });
+        break;
+    }
   } catch (error) {
     res.json({
       code: 400,
